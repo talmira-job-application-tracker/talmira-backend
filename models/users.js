68f2a6cad0bdd87f00e2578cbeae2a346033acb4
Shehgit <shehashehanas@gmail.com>
2025-08-01 13:09:12 +0530

    added users.js

---
+++ models/users.js
@@ -0,0 +1,45 @@
+import mongoose,{Schema} from "mongoose";
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    role: {
+        type: String,
+        enum:["admin","user"],
+        default: "user"
+    },
+    image:{
+        type: String,
+        default: null,
+    },
+    age: {
+        type: Number,
+        min: 0,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+},
+    {
+    timestamps: true
+    }
+);
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
