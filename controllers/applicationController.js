efeebbb8be59d8bee4dda6aec41177d43b633062
Shehgit <shehashehanas@gmail.com>
2025-08-06 12:59:57 +0530

    view and list applications

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -50,3 +50,71 @@ export const createApplication = async (req, res, next) => {
         return next(new HttpError("Failed to submitted", 500));
     }
 };
+
+//view
+export const viewApplication = async (req, res, next) => {
+    try{
+         const { id } = req.params;
+         const { user_role } = req.userData;
+
+         if (user_role !== "admin") {
+            return next(new HttpError("Only admins have access to view", 422));
+         }
+
+         let getApplication = await Application.findById(id)
+         .select("user job appliedAt resume contactInfo")
+         .populate([{
+            path: "user",
+            select: "name"
+         },{
+            path: "job",
+            select: "title company"
+         }])
+
+         if(!getApplication) {
+            return next(new HttpError("Application not found", 404));
+         }
+
+        res.status(200).json({
+            status: true,
+            message: "success",
+            data: getApplication
+        });
+
+    } catch (err) {
+        return next(new HttpError("Failed to fetch application details", 500));
+    }
+}
+
+//list
+export const listApplication = async(req, res, next) => {
+    try{
+        const {user_role} = req.userData;
+        if (user_role !== "admin") {
+            return next(new HttpError("Only admins have access to view", 422));
+         }
+
+        const listedApplications = await Application.find()
+        .select("user job appliedAt status resume contactInfo")
+        .populate([{
+            path: "user",
+            select: "name"
+         },{
+            path: "job",
+            select: "title company"
+         }])
+
+         if(!listedApplications) {
+            return next(new HttpError("No applications found", 404));
+         }
+
+        res.status(200).json({
+            status: true,
+            message: "success",
+            data: listedApplications
+        });
+
+    } catch(err) {
+        return next(new HttpError("Failed to fetch application details", 500));
+    }
+}
